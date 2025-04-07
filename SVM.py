c3ac919e87bd08381f99fb99a532de8a1a086142
luchtele <lea.luchterhand@gmail.com>
2025-04-07 15:40:10 -0400

    experimenting with different features

--- SVM.py
+++ SVM.py
@@ -27,8 +27,10 @@ from sklearn.model_selection import GridSearchCV
 
 
 def clean_data(df):
-    remove_numeric_columns = ['Unnamed: 0', 'shotID', 'game_id', 'id', 'goalieIdForShot','shooterPlayerId', 'HOMEZONE', 'AWAYZONE','location_Unknown','xCord','yCord','shotGeneratedRebound','homeTeamGoals','awayTeamGoals','xGoal', 'xFroze','xRebound','xPlayContinuedInZone','xPlayContinuedOutsideZone','xShotWasOnGoal', 'shotAngle', 'shotAngleReboundRoyalRoad','defendingTeamDefencemenOnIce', 'timeDifferenceSinceChange', 'averageRestDifference']
+    remove_numeric_columns = ['Unnamed: 0', 'shotID', 'game_id', 'id', 'goalieIdForShot','shooterPlayerId', 'HOMEZONE', 'AWAYZONE','location_Unknown','xCord','yCord','shotGeneratedRebound','homeTeamGoals','awayTeamGoals','xGoal', 'xFroze','xRebound','xPlayContinuedInZone','xPlayContinuedOutsideZone','xShotWasOnGoal', 'shotAngle', 'shotAngleReboundRoyalRoad','defendingTeamDefencemenOnIce', 'timeDifferenceSinceChange', 'averageRestDifference','goal','season', 'homeTeamWon','shotPlayContinuedOutsideZone', 'shotPlayContinuedInZone','shotPlayStopped', 'speedFromLastEvent', 'lastEventxCord', 'lastEventyCord', 'homeEmptyNet', 'awayEmptyNet', 'homeSkatersOnIce', 'awaySkatersOnIce', 'awayPenalty1TimeLeft', 'awayPenalty1Length', 'homePenalty1TimeLeft', 'homePenalty1Length', 'playerNumThatDidEvent', 'playerNumThatDidLastEvent', 'offWing', 'arenaAdjustedShotDistance', 'arenaAdjustedXCord', 'arenaAdjustedYCord', 'arenaAdjustedYCordAbs', 'xPlayStopped', 'isHomeTeam', 'arenaAdjustedXCordABS', 'location_AWAYZONE', 'location_HOMEZONE', 'location_Neu. Zone']
 #'xCord','yCord','shotGeneratedRebound','homeTeamGoals','awayTeamGoals','xGoal', 'xFroze','xRebound','xPlayContinuedInZone','xPlayContinuedOutsideZone', 'PlayStopped','xShotWasOnGoal', 'shotAngle', 'shotAngleReboundRoyalRoad','goal'
+
+# 'season', 'homeTeamWon','shotPlayContinuedOutsideZone', 'shotPlayContinuedInZone','shotPlayStopped', 'speedFromLastEvent', 'lastEventxCord', 'lastEventyCord', 'homeEmptyNet', 'awayEmptyNet', 'homeSkatersOnIce', 'awaySkatersOnIce', 'awayPenalty1TimeLeft', 'awayPenalty1Length', 'homePenalty1TimeLeft', 'homePenalty1Length', 'playerNumThatDidEvent', 'playerNumThatDidLastEvent', 'offWing', 'arenaAdjustedShotDistance', 'arenaAdjustedXCord', 'arenaAdjustedYCord', 'arenaAdjustedYCordAbs', 'xPlayStopped', 'isHomeTeam', 'arenaAdjustedXCordABS', 'location_AWAYZONE', 'location_HOMEZONE', 'location_Neu. Zone'
 
     cleaned_df = df.select_dtypes(include=np.number)
     return cleaned_df.drop(columns=remove_numeric_columns)
@@ -48,52 +50,83 @@ def remove_dep_with_qr(df: pd.DataFrame, tol=1e-9) -> pd.DataFrame:
 if __name__ == '__main__':
     
     base_path = os.getcwd()
-    path_train = os.path.join(base_path,'MATH7243','data', 'training_data.csv') 
-    path_valid = f'{base_path}\\MATH7243\\data\\validation_data.csv'
-    fig_save_path = f'{base_path}\\MATH7243\\plots\\logistic_regression'
+    path_train = os.path.join(base_path,'data', 'training_data.csv') 
+    path_valid = f'{base_path}\\data\\validation_data.csv'
+    fig_save_path = f'{base_path}\\plots\\logistic_regression'
     y_col = 'goal'
 
     # process data
-    df_train = pd.read_csv(path_train, nrows= 3000)
-    df_valid = pd.read_csv(path_valid, nrows=3000)
+    df_trainunsampled = pd.read_csv(path_train)
+    df_validunsampled = pd.read_csv(path_valid)
+
+    df_train_1= df_trainunsampled[df_trainunsampled['goal'] == 1]
+    df_train_0= df_trainunsampled[df_trainunsampled['goal'] == 0]
+
+    dftrain_1s= df_train_1.sample(n=15000, random_state=42)
+    dftrain_0s= df_train_0.sample(n=15000, random_state=42)
+    df_train= pd.concat([dftrain_1s, dftrain_0s], ignore_index=True)
+
+    df_valid_1= df_trainunsampled[df_trainunsampled['goal'] == 1]
+    df_valid_0= df_validunsampled[df_validunsampled['goal'] == 0]
+    dfvalid_1s= df_valid_1.sample(n=1000, random_state=42)
+    dfvalid_0s= df_valid_0.sample(n=1000, random_state=42)
+
+    df_valid= pd.concat([dfvalid_1s, dfvalid_0s], ignore_index=True)
+
+
     df_train_cleaned = clean_data(df_train)
     df_valid_cleaned = clean_data(df_valid)
+    
     y_train, y_valid = df_train[y_col].values, df_valid[y_col].values
 
-    X_train, X_valid = df_train_cleaned.values, df_valid_cleaned.values
+    #X_train, X_valid = df_train_cleaned.values, df_valid_cleaned.values
+
     ####################################
  
 
-  #  required_columns = ['period', 'shotGoalieFroze', 'shotPlayStopped', 'ShotType', 'shotOnEmptyNet', 'shotRebound',                    'shotWasOnGoal', 'time', 'timeDifferenceSinceChange', 'averageRestDifference', 'timeSinceLastEvent',
-       #             'speedFromLastEvent', 'lastEventxCord', 'lastEventyCord', 'distanceFromLastEvent', 'lastEventShotAngle',
-        #            'lastEventShotDistance', 'LastEventCategory', 'shooterTimeOnIce', 'ShooterLeftRight', 
-         #           'PlayerPositionThatDidEvent', 'defendingTeamForwardsOnIce', 'defendingTeamDefencemenOnIce', 
-          #          'defendingTeamAverageTimeOnIce', 'defendingTeamAverageTimeOnIceOfForwards', 
-           #         'defendingTeamAverageTimeOnIceOfDefencemen', 'defendingTeamAverageTimeOnIceSinceFaceoff', 
-            #        'shootingTeamForwardsOnIce', 'shootingTeamDefencemenOnIce', 'shootingTeamAverageTimeOnIce', 
-             #       'shootingTeamAverageTimeOnIceOfForwards', 'shootingTeamAverageTimeOnIceOfDefencemen', 
-              #      'shotAngle', 'shotDistance']
+    #required_columns = ['period', 'shotGoalieFroze', 'shotPlayStopped', 'ShotType', 'shotOnEmptyNet', 'shotRebound', 'shotWasOnGoal', 'time', 'timeDifferenceSinceChange', 'averageRestDifference', 'timeSinceLastEvent', 'speedFromLastEvent', 'lastEventxCord', 'lastEventyCord', 'distanceFromLastEvent', 'lastEventShotAngle',           'lastEventShotDistance', 'LastEventCategory', 'shooterTimeOnIce', 'ShooterLeftRight',            'PlayerPositionThatDidEvent', 'defendingTeamForwardsOnIce', 'defendingTeamDefencemenOnIce',          'defendingTeamAverageTimeOnIce', 'defendingTeamAverageTimeOnIceOfForwards',        'defendingTeamAverageTimeOnIceOfDefencemen', 'defendingTeamAverageTimeOnIceSinceFaceoff',        'shootingTeamForwardsOnIce', 'shootingTeamDefencemenOnIce', 'shootingTeamAverageTimeOnIce',        'shootingTeamAverageTimeOnIceOfForwards', 'shootingTeamAverageTimeOnIceOfDefencemen',      'shotAngle', 'shotDistance']
 
-  #  available_columns = [col for col in required_columns if col in df_train.columns]
-   # dfnew= df_train[available_columns]
+    #available_columns = [col for col in required_columns if col in df_train.columns]
+    #dfnew= df_train[available_columns]
 
    # dfnewvalid= df_valid[available_columns]
-   # y_train, y_valid = df_train[y_col].values, df_valid[y_col].values
-    #X_train, X_valid = dfnew.values, dfnewvalid.values
+  #  y_train, y_valid = df_train[y_col].values, df_valid[y_col].values
+##    X_train, X_valid = dfnew.values, dfnewvalid.values
 
-indep= remove_dep_with_qr(df_train_cleaned) 
+#indep= remove_dep_with_qr(df_train_cleaned) 
 
 
+features= ['isPlayoffGame', 'time', 'timeUntilNextEvent', 'timeSinceLastEvent', 'period', 'shotGoalieFroze', 'shotType_BACK', 'shotType_DEFL', 'shotType_SLAP', 'shotType_SNAP', 'shotType_TIP', 'shotType_WRAP', 'shotType_WRIST', 'xCordAdjusted', 'yCordAdjusted', 'shotAngleAdjusted', 'shotAnglePlusRebound', 'shotDistance', 'shotOnEmptyNet', 'shotRebound', 'shotAnglePlusReboundSpeed', 'shotRush', 'distanceFromLastEvent', 'lastEventShotAngle', 'lastEventShotDistance', 'lastEventxCord_adjusted', 'lastEventyCord_adjusted', 'timeSinceFaceoff', 'shooterTimeOnIce', 'shooterTimeOnIceSinceFaceoff', 'shootingTeamForwardsOnIce', 'shootingTeamDefencemenOnIce', 'shootingTeamAverageTimeOnIce', 'shootingTeamAverageTimeOnIceOfForwards', 'shootingTeamAverageTimeOnIceOfDefencemen', 'shootingTeamMaxTimeOnIce', 'shootingTeamMaxTimeOnIceOfForwards', 'shootingTeamMaxTimeOnIceOfDefencemen', 'shootingTeamMinTimeOnIce', 'shootingTeamMinTimeOnIceOfForwards', 'shootingTeamMinTimeOnIceOfDefencemen', 'shootingTeamAverageTimeOnIceSinceFaceoff', 'shootingTeamAverageTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamAverageTimeOnIceOfDefencemenSinceFaceoff', 'shootingTeamMaxTimeOnIceSinceFaceoff', 'shootingTeamMaxTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamMaxTimeOnIceOfDefencemenSinceFaceoff', 'shootingTeamMinTimeOnIceSinceFaceoff', 'shootingTeamMinTimeOnIceOfForwardsSinceFaceoff', 'shootingTeamMinTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamForwardsOnIce', 'defendingTeamAverageTimeOnIce', 'defendingTeamAverageTimeOnIceOfForwards', 'defendingTeamAverageTimeOnIceOfDefencemen', 'defendingTeamMaxTimeOnIce', 'defendingTeamMaxTimeOnIceOfForwards', 'defendingTeamMaxTimeOnIceOfDefencemen', 'defendingTeamMinTimeOnIce', 'defendingTeamMinTimeOnIceOfForwards', 'defendingTeamMinTimeOnIceOfDefencemen', 'defendingTeamAverageTimeOnIceSinceFaceoff', 'defendingTeamAverageTimeOnIceOfForwardsSinceFaceoff', 'defendingTeamAverageTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamMaxTimeOnIceSinceFaceoff', 'defendingTeamMaxTimeOnIceOfForwardsSinceFaceoff', 'defendingTeamMaxTimeOnIceOfDefencemenSinceFaceoff', 'defendingTeamMinTimeOnIceSinceFaceoff', 'defendingTeamMinTimeOnIceOfForwardsSinceFaceoff', 'defendingTeamMinTimeOnIceOfDefencemenSinceFaceoff', 'shotWasOnGoal','goal']
+feat= features
+feat = [f for f in features if f != 'goal']
+selected_features = []
+for f in feat:
+    selected_features.append(f)  
+    X_train = df_train[selected_features].values
+    X_valid = df_valid[selected_features].values
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_valid_scaled = scaler.fit_transform(X_valid)
+
+    svm_clf = SVC(kernel="linear",gamma='scale', C=10)
+    svm_clf.fit(X_train_scaled, y_train)
+    y_pred = svm_clf.predict(X_valid_scaled)
+    score = svm_clf.score(X_valid_scaled, y_valid)
+    if score > 0.9:
+        print(f)
+        selected_features.remove(f)
+
+#goalie froze
     
-rank= np.linalg.matrix_rank(X_train)
+#rank= np.linalg.matrix_rank(X_train)
 
 
 
-scaler = StandardScaler()
 
 
-X_train_scaled = scaler.fit_transform(X_train)
-X_valid_scaled = scaler.fit_transform(X_valid)
+
+
+
 
 
 
@@ -101,12 +134,13 @@ X_valid_scaled = scaler.fit_transform(X_valid)
 
 
 
-svm_clf = SVC(kernel="linear",gamma='scale', C=1)
-svm_clf.fit(X_train_scaled, y_train)
+#svm_clf = SVC(kernel="linear",gamma='scale', C=10)
+#svm_clf.fit(X_train_scaled, y_train)
 
 ######################Predictions
-y_pred = svm_clf.predict(X_valid_scaled)
+#y_pred = svm_clf.predict(X_valid_scaled)
 
+#score = svm_clf.score(X_valid_scaled, y_valid)
 
-print("Accuracy:", svm_clf.score(X_valid_scaled, y_valid))
+#print("Accuracy:", score)
 #,class_weight='balanced'
